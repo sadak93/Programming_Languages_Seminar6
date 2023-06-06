7fce13e50ec1e02ac56d218b5f95f09673380d7f
Sadak <s.sadak02@gmail.com>
2023-06-06 17:54:20 +0600

    Десятичное в двоичное

---
+++ Task042/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+string Res(int N)
+{
+    string res = "";
+    while (N > 0)
+    {
+        res = Convert.ToString(N % 2) + res;
+        N = N / 2;
+    }
+    return res;
+}
+System.Console.WriteLine("Введите двоичное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Res(N));
